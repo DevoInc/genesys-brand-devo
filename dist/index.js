6897cca8a2551db87c11a273fa15d42600baf96c
Javier López Úbeda <jlopezcur@gmail.com>
2024-11-28 18:05:05 +0100

    build: moved to ESM the internal files

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,19 @@
-'use strict';
+import lightCmp from "./light/js/tokens.esm.cmp.js";
+import lightAlias from "./light/js/tokens.esm.alias.js";
+import lightMeta from "./light/js/tokens.esm.meta.js";
 
-module.exports = {
-  light: {
-    ...require('./light/js/tokens.module.reduced.cmp.min.js'),
-    ...require('./light/js/tokens.module.reduced.alias.min.js'),
-    ...require('./light/js/tokens.module.reduced.meta.js'),
-  },
-  dark: {
-    ...require('./dark/js/tokens.module.reduced.cmp.min.js'),
-    ...require('./dark/js/tokens.module.reduced.alias.min.js'),
-    ...require('./dark/js/tokens.module.reduced.meta.js'),
-  },
+import darkCmp from "./dark/js/tokens.esm.cmp.js";
+import darkAlias from "./dark/js/tokens.esm.alias.js";
+import darkMeta from "./dark/js/tokens.esm.meta.js";
+
+export const light = {
+  ...lightCmp,
+  ...lightAlias,
+  ...lightMeta,
+};
+
+export const dark = {
+  ...darkCmp,
+  ...darkAlias,
+  ...darkMeta,
 };
